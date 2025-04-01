a3df9042b6156b4050cd8dd6e2455b8b48d58707
Luca Witt <witt-luca@web.de>
2025-04-01 12:07:50 +0200

    Basic Flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "OpenGL programming environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # OpenGL dependencies
+            xorg.libX11
+            xorg.libXrandr
+            xorg.libXinerama
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXext
+            libGL
+            glew
+            glfw
+
+            glxinfo
+
+            # Build tools (customize as needed)
+            gnumake
+            pkg-config
+          ];
+
+          shellHook = ''
+            export LD_LIBRARY_PATH="/run/opengl-driver/lib:/run/opengl-driver-32/lib:$LD_LIBRARY_PATH"
+          '';
+        };
+      }
+    );
+}
